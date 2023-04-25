c9dcba6861beafedf7bb299a7d19ea673e44df53
David Dahlgren <dadh22@student.bth.se>
2023-04-25 13:13:24 +0200

    minor code tweak

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -2,18 +2,6 @@
 
 namespace App\Card;
 
-// class Card {
-//     const SPADES = 0;   // Codes for the 4 suits, plus Joker.
-//     const HEARTS = 1;
-//     const DIAMONDS = 2;
-//     const CLUBS = 3;
-
-//     const ACE = 1;      // Codes for the non-numeric cards.
-//     const JACK = 11;    //   Cards 2 through 10 have their
-//     const QUEEN = 12;   //   numerical values for their codes.
-//     const KING = 13;
-
-// }
 class Card
 {
     public const COLOR1 ='red';
